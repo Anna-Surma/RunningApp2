fe93a39527423a2ce424c73961b818280f6eabb1
Anna-Surma <anna.surma2000@gmail.com>
2022-10-04 14:59:25 +0200

    Makes Foreground Service Notification Manager Notification navigation

--- app/src/main/java/com/example/runningapp2/MainActivity.kt
+++ app/src/main/java/com/example/runningapp2/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.runningapp2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.runningapp2.databinding.ActivityMainBinding
+import com.example.runningapp2.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +26,8 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
                 when(destination.id) {
@@ -31,5 +36,17 @@ class MainActivity : AppCompatActivity() {
                     else -> binding.bottomNavigationView.visibility = View.GONE
                 }
             }
+        navigateToTrackingFragmentIfNeeded(intent)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navigateToTrackingFragmentIfNeeded(intent)
+    }
+
+    private fun navigateToTrackingFragmentIfNeeded(intent: Intent?) {
+        if(intent?.action == ACTION_SHOW_TRACKING_FRAGMENT) {
+            navController.navigate(R.id.action_global_trackingFragment)
+        }
     }
 }
--- app/src/main/java/com/example/runningapp2/models/TrackingService.kt
+++ app/src/main/java/com/example/runningapp2/models/TrackingService.kt
@@ -1,19 +1,40 @@
 package com.example.runningapp2.models
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.NotificationManager.IMPORTANCE_LOW
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
+import com.example.runningapp2.MainActivity
+import com.example.runningapp2.R
+import com.example.runningapp2.other.Constants
 import com.example.runningapp2.other.Constants.ACTION_PAUSE_SERVICE
 import com.example.runningapp2.other.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.example.runningapp2.other.Constants.ACTION_STOP_SERVICE
+import com.example.runningapp2.other.Constants.NOTIFICATION_CHANNEL_ID
+import com.example.runningapp2.other.Constants.NOTIFICATION_ID
+import androidx.annotation.RequiresApi
+import com.example.runningapp2.other.Constants.NOTIFICATION_CHANNEL_NAME
 
 class TrackingService: LifecycleService() {
+
+    var isFirstRun = true
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
             when(it.action) {
                 ACTION_START_OR_RESUME_SERVICE -> {
-                    Log.i("Tracking Service", "Started or resumed service")
+                    if (isFirstRun) {
+                        startForegroundService()
+                        isFirstRun = false
+                    }
+                    else
+                        Log.i("Tracking Service", "Started or resumed service")
                 }
                 ACTION_PAUSE_SERVICE -> {
                     Log.i("Tracking Service","Paused service")
@@ -25,5 +46,44 @@ class TrackingService: LifecycleService() {
             }
         }
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun startForegroundService(){
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE)
+        as NotificationManager
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            createNotificationChannel(notificationManager)
+        }
+
+        val notificationBuilder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setAutoCancel(false)
+            .setOngoing(true)
+            .setSmallIcon(R.drawable.ic_directions_run_black_24dp)
+            .setContentTitle("Running App")
+            .setContentText("00:00:00")
+            .setContentIntent(getMainActivityPendingIntent())
+            .build()
+
+        startForeground(NOTIFICATION_ID, notificationBuilder)
+    }
+
+    private fun getMainActivityPendingIntent() = PendingIntent.getActivity(
+        this,
+        0,
+        Intent(this, MainActivity::class.java).also {
+            it.action = Constants.ACTION_SHOW_TRACKING_FRAGMENT
+        },
+        PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+    )
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(notificationManager: NotificationManager){
+        val channel = NotificationChannel(
+            NOTIFICATION_CHANNEL_ID,
+            NOTIFICATION_CHANNEL_NAME,
+            IMPORTANCE_LOW
+        )
+        notificationManager.createNotificationChannel(channel)
     }
 }
